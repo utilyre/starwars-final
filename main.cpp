acb14fa1e2c4c2c659c3e7fc138cdb79c82e83fd
Amirabbas Fazelinia <utilyre@gmail.com>
2024-02-09 20:54:43 +0330

    feat: implement rendering and player movement

--- main.cpp
+++ main.cpp
@@ -1,13 +1,77 @@
 #include <iostream>
 #include <ncurses.h>
+#include <algorithm>
 
-int main() {
+const int SIZE = 20;
+
+struct Vec2
+{
+    int y, x;
+
+    Vec2(int y, int x) : y(y), x(x) {}
+};
+
+class Game
+{
+public:
+    Game() : m_player_translation(Vec2(SIZE - 1, SIZE / 2)) {}
+
+    void render() const
+    {
+        for (int i = 0; i < SIZE; i++)
+        {
+            for (int j = 0; j < SIZE; j++)
+            {
+                mvprintw(i, 2 * j, " ");
+                if (m_player_translation.y == i && m_player_translation.x == j)
+                {
+                    printw("#");
+                }
+                else
+                {
+                    printw("-");
+                }
+            }
+        }
+    }
+
+    void move_player(int dy, int dx)
+    {
+        m_player_translation.y = std::clamp(m_player_translation.y + dy, 0, SIZE - 1);
+        m_player_translation.x = std::clamp(m_player_translation.x + dx, 0, SIZE - 1);
+    }
+
+private:
+    Vec2 m_player_translation;
+};
+
+int main()
+{
+    Game game;
+
     initscr();
     noecho();
     curs_set(0);
 
-    printw("Hello world");
-    getch();
+    // collision_map
+    // enemies
+    // shots
+    // player_x
+
+    while (true)
+    {
+        game.render();
+
+        switch (getch())
+        {
+        case 'a':
+            game.move_player(0, -1);
+            break;
+        case 'd':
+            game.move_player(0, 1);
+            break;
+        }
+    }
 
     endwin();
 }
