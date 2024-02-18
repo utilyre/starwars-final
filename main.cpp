8dbcb80e46dae82fe8dd36d54a61ef2031d1e0e8
Amirabbas Fazelinia <utilyre@gmail.com>
2024-02-18 15:11:52 +0330

    feat(main): take size and max score from user

--- main.cpp
+++ main.cpp
@@ -4,6 +4,7 @@
 #include "colors.h"
 #include "menu.h"
 #include "game.h"
+#include "utils.h"
 
 void init();
 void open_menu();
@@ -58,6 +59,16 @@ void action_continue(Menu &menu)
 
 void action_new(Menu &menu)
 {
+    menu.stop();
+
+    char size_str[8];
+    inputn("Size:", Vec2((LINES - 3) / 2, (COLS - 8) / 2), 8, size_str);
+
+    char max_score_str[16];
+    inputn("Max Score:", Vec2((LINES - 3) / 2, (COLS - 16) / 2), 16, max_score_str);
+
+    Game game(std::atoi(size_str));
+    game.start();
 }
 
 void action_quit(Menu &menu)
